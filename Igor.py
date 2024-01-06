cc5a0e8b76969ca68fd183e19c3a0d36b5fc7491
Baxbox <info@baxbox.com>
2024-01-06 15:47:33 -0500

    Added emotions and logging

--- Igor.py
+++ Igor.py
@@ -7,13 +7,14 @@ class Igor:
 
     class Eyes:
 
-        def __init__(self,lid='',rid=''):
+        def __init__(self,lid='',rid='',maxBrightness=1,rate=0):
             self.lled = PWMLED(lid)
             self.rled = PWMLED(rid)
+            self.rate = rate
             self.brightnessSteps = 20
-            self.brightnessInc = 1 / self.brightnessSteps
+            self.brightnessInc = maxBrightness / self.brightnessSteps
 
-        def open(self,rate=0):
+        def open(self):
             #Go from off to on at rate
             brightness = 0 #Default brightness
             count = 0
@@ -21,13 +22,13 @@ class Igor:
             while count <= self.brightnessSteps:
                 self.lled.value = brightness
                 self.rled.value = brightness             
-                sleep(rate)
+                sleep(self.rate)
                 count = count + 1
                 brightness = brightness + self.brightnessInc
                 if brightness >= 1:
                     brightness = 1 #gpiozero does not like 1.0
 
-        def close(self,rate=0):
+        def close(self):
             #Go from on to off at rate
             brightness = 1 #Default brightness
             count = 0
@@ -35,17 +36,37 @@ class Igor:
             while count <= self.brightnessSteps:
                 self.lled.value = brightness
                 self.rled.value = brightness             
-                sleep(rate)
+                sleep(self.rate)
                 count = count + 1
                 brightness = brightness - self.brightnessInc
                 if brightness <= 0:
                     brightness = 0 #gpiozero does not like < 0
 
-        def blink(self,rate=0):
+        def blink(self):
             #Go from open to close to open at rate
-            self.open(rate)
+            self.open()
             sleep(.3)
-            self.close(rate)
+            self.close()
             sleep(.3)
-            self.open(rate)
+            self.open()
+
+    class Legs:
+
+            def __init__(self,gpio=''):
+                self.gpio = gpio
+                pass
+
+            def rotate(degrees=30):
+                #todo
+                pass
+
+    class Arms:
+
+            def __init__(self,gpio=''):
+                self.gpio = gpio
+                pass
+
+            def rotate(degrees=30):
+                #todo
+                pass
 
